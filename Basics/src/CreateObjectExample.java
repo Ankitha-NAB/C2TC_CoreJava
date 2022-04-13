379650ab72401ac902225fc99c4caf261ab9e401
Admin <Admin@Ankitha>
2022-04-13 12:45:59 +0530

    First Commit

---
+++ Basics/src/CreateObjectExample.java
@@ -0,0 +1,13 @@
+public class CreateObjectExample   
+{    
+void show()    
+{    
+System.out.println("Welcome to javaTpoint");    
+}    
+public static void main(String[] args)   
+{    
+
+CreateObjectExample obj = new CreateObjectExample();   
+obj.show();    
+}    
+}   
