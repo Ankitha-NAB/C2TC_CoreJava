85f2e5ec61d09d144845411220edc87035cec91f
Admin <Admin@Ankitha>
2022-06-12 12:03:09 +0530

    loop

---
+++ looping statements/src/loopstatements/DoWhileLoop.java
@@ -0,0 +1,14 @@
+package loopstatements;
+
+public class DoWhileLoop {
+
+	public static void main(String[] args) {
+		int x=1;
+		do {
+			System.out.println(x);
+			x++;
+		}while(x<=5);
+
+	}
+
+}
---
+++ looping statements/src/loopstatements/WhileLoop.java
@@ -0,0 +1,14 @@
+package loopstatements;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		int n=1;
+		while(n<=5) {
+			System.out.println(n);
+			n++;
+		}
+
+	}
+
+}
